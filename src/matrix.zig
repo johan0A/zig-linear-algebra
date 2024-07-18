f6dbeca34a2e204b2017b03c0192252e0234dde0
johan0A <arnaudchesse@outlook.com>
2024-07-18 16:54:06 +0200

    added matrix multiplication function and optimized to ~15x speed for large matrices (from naive implementation) no improvement for small matrices

--- src/matrix.zig
+++ src/matrix.zig
@@ -17,5 +17,56 @@ pub fn Matrix(comptime T: type, comptime rows: usize, comptime cols: usize) type
                 .data = values,
             };
         }
+
+        pub fn mul(self: Self, other: anytype) Self {
+            if (@TypeOf(self).cols_count != @TypeOf(other).rows_count) {
+                @compileError("Number of columns of self must be equal to number of rows of other");
+            }
+
+            var result: Self = undefined;
+            for (0..result.data.len) |i| {
+                for (0..result.data[i].len) |j| {
+                    result.data[i][j] = 0;
+                    for (0..self.data[i].len) |k| {
+                        result.data[i][j] += self.data[i][k] * other.data[k][j];
+                    }
+                }
+            }
+
+            return result;
+        }
     };
+}
+
+test "Matrix multiplication" {
+    const a = Matrix(f32, 2, 2).init(.{
+        .{ 1.0, 2.0 },
+        .{ 3.0, 4.0 },
+    });
+    const b = Matrix(f32, 2, 2).init(.{
+        .{ 5.0, 6.0 },
+        .{ 7.0, 8.0 },
+    });
+    const excpected_c = Matrix(f32, 2, 2).init(.{
+        .{ 19.0, 22.0 },
+        .{ 43.0, 50.0 },
+    });
+    const c = a.mul(b);
+    try std.testing.expectEqual(excpected_c, c);
+
+    const d = Matrix(f32, 2, 3).init(.{
+        .{ 1.0, 2.0, 3.0 },
+        .{ 4.0, 5.0, 6.0 },
+    });
+    const e = Matrix(f32, 3, 3).init(.{
+        .{ 1.0, 2.0, 3.0 },
+        .{ 4.0, 5.0, 6.0 },
+        .{ 7.0, 8.0, 9.0 },
+    });
+    const excpected_f = Matrix(f32, 2, 3).init(.{
+        .{ 30, 36, 42 },
+        .{ 66, 81, 96 },
+    });
+    const f = d.mul(e);
+    try std.testing.expectEqual(excpected_f, f);
 }
