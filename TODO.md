c22dad41d490c6036bc379327f2284fa5284dd0e
johan0A <arnaudchesse@outlook.com>
2025-04-17 05:19:25 +0200

    fixed and optimized matmul

--- TODO.md
+++ TODO.md
@@ -5,11 +5,6 @@
 - [x] add doc comments for Vector operations
 - [x] add more operations to Vectors
 - [x] rework how precision of output types is handled for vectors again
-- [ ] decide how to handle undefined behavior (eg overflows)
+- [ ] Add more operations to Matrices
 - [ ] add install guide
 - [ ] add usage guide
-- [ ] rework how precision of output types is handle for matrices
-- [ ] rework how matrices handle integer types
-- [ ] add even more operations to Vectors
-- [ ] Add more operations to Matrices
-- [ ] Compare performance to openblas
--- src/matrix.zig
+++ src/matrix.zig
@@ -1,36 +1,38 @@
 const std = @import("std");
 
-pub fn Mat(comptime T: type, comptime rows: usize, comptime cols: usize) type {
+pub fn Mat(comptime T: type, comptime cols: usize, comptime rows: usize) type {
     return struct {
         const Self = @This();
 
-        const alignement = @alignOf(@Vector(std.simd.suggestVectorLength(T) orelse 1, T));
-        const DataType = [rows][cols]T;
+        const alignement = @alignOf(@Vector(rows, T));
 
-        items: DataType align(alignement),
+        items: [cols][rows]T align(alignement),
+
         comptime rows: comptime_int = rows,
         comptime cols: comptime_int = cols,
         comptime Type: type = T,
 
-        pub fn init(values: DataType) Self {
+        pub fn init(values: [cols][rows]T) Self {
             return Self{
                 .items = values,
             };
         }
 
-        pub fn mul(self: Self, other: anytype) Self {
+        pub fn mul(self: Self, other: anytype) Mat(T, other.cols, self.rows) {
             if (self.cols != other.rows) @compileError("Number of columns of self must be equal to number of rows of other");
             if (self.Type != other.Type) @compileError("Type of self must be equal to Type of other");
 
-            var result: Self = undefined;
-            for (0..result.items.len) |i| {
-                for (0..result.items[i].len) |j| {
-                    result.items[i][j] = 0;
-                    for (0..self.items[i].len) |k| {
-                        result.items[i][j] += self.items[i][k] * other.items[k][j];
-                    }
+            const Wt = @Vector(rows, T);
+
+            var result: Mat(T, other.cols, self.rows) = undefined;
+
+            for (0..result.cols) |i| {
+                result.items[i] = @as(Wt, self.items[0]) * @as(Wt, @splat(other.items[i][0]));
+                for (1..self.cols) |j| {
+                    result.items[i] = @as(Wt, result.items[i]) + @as(Wt, self.items[j]) * @as(Wt, @splat(other.items[i][j]));
                 }
             }
+
             return result;
         }
 
@@ -43,37 +45,40 @@ pub fn Mat(comptime T: type, comptime rows: usize, comptime cols: usize) type {
 test "Matrix multiplication" {
     {
         const a = Mat(f32, 2, 2).init(.{
-            .{ 1.0, 2.0 },
-            .{ 3.0, 4.0 },
+            .{ 1, 2 },
+            .{ 3, 4 },
         });
         const b = Mat(f32, 2, 2).init(.{
-            .{ 5.0, 6.0 },
-            .{ 7.0, 8.0 },
-        });
-        const excpected_c = Mat(f32, 2, 2).init(.{
-            .{ 19.0, 22.0 },
-            .{ 43.0, 50.0 },
+            .{ 5, 6 },
+            .{ 7, 8 },
         });
         const c = a.mul(b);
+
+        const excpected_c = Mat(f32, 2, 2).init(.{
+            .{ 23, 34 },
+            .{ 31, 46 },
+        });
         try std.testing.expectEqual(excpected_c, c);
     }
 
     {
         const a = Mat(f32, 2, 3).init(.{
-            .{ 1.0, 2.0, 3.0 },
-            .{ 4.0, 5.0, 6.0 },
+            .{ 1, 2, 3 },
+            .{ 4, 5, 6 },
         });
-        const b = Mat(f32, 3, 3).init(.{
-            .{ 1.0, 2.0, 3.0 },
-            .{ 4.0, 5.0, 6.0 },
-            .{ 7.0, 8.0, 9.0 },
-        });
-        const excpected_f = Mat(f32, 2, 3).init(.{
-            .{ 30, 36, 42 },
-            .{ 66, 81, 96 },
+        const b = Mat(f32, 3, 2).init(.{
+            .{ 1, 2 },
+            .{ 3, 4 },
+            .{ 5, 6 },
         });
         const c = a.mul(b);
-        try std.testing.expectEqual(excpected_f, c);
+
+        const excpected_c = Mat(f32, 3, 3).init(.{
+            .{ 9, 12, 15 },
+            .{ 19, 26, 33 },
+            .{ 29, 40, 51 },
+        });
+        try std.testing.expectEqual(excpected_c, c);
     }
 
     {
@@ -89,13 +94,14 @@ test "Matrix multiplication" {
             .{ 25, 26, 27, 28 },
             .{ 29, 30, 31, 32 },
         });
-        const excpected_c = Mat(f32, 4, 4).init(.{
-            .{ 250, 260, 270, 280 },
-            .{ 6.18e2, 6.44e2, 6.7e2, 6.96e2 },
-            .{ 9.86e2, 1.028e3, 1.07e3, 1.112e3 },
-            .{ 1.354e3, 1.412e3, 1.47e3, 1.528e3 },
-        });
         const c = a.mul(b);
+
+        const excpected_c = Mat(f32, 4, 4).init(.{
+            .{ 538, 612, 686, 760 },
+            .{ 650, 740, 830, 920 },
+            .{ 762, 868, 974, 1080 },
+            .{ 874, 996, 1118, 1240 },
+        });
         try std.testing.expectEqual(excpected_c, c);
     }
 }
